cb433568cc458a165126cf6bb46c55dea64e39ee
sqx <ugo.mare.epitech@gmail.com>
2014-07-27 14:37:16 +0200

    First

---
+++ BlogReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlogReader
+//
+//  Created by Ugo on 27/07/2014.
+//  Copyright (c) 2014 Ugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
